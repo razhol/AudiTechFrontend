058ddad215803d19365ca62f0ec0c4ac77375a31
raz <razholden11@gmail.com>
2022-01-19 17:26:44 +0200

    frontend commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
 
 function App() {
+
+  const [pullRequests, setPullRequests] = useState([])
+
+  useEffect(() => {
+    async function getDateFromApi() {
+      let dataGetRequest = await axios.get("http://localhost:8000/getPullRequest")
+      setPullRequests(dataGetRequest.data)
+    }
+    getDateFromApi()
+    const interval = setInterval(() => getDateFromApi(), 10000)
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
+
+
+  const convertTodate = function (date) {
+    let year = date.split("T")
+    console.log(year)
+    return year[0]
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <h3>pull requests list</h3>
+      {
+        pullRequests.map(x => {
+          return <div id="pullRequest" key={x.id}>
+            <p>title: {x.title}</p>
+            <p>body: {x.body}</p>
+            <p>created date: {convertTodate(x.created_at)}</p>
+            <p>url: {x.url}</p>
+          </div>
+        })
+      }
     </div>
   );
 }
